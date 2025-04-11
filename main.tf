bb9dd507f3a40dea1c8c4d9d943fdb09cd35465a
OzanEFETopcu <ozanefe.topcu@gmail.com>
2025-04-11 22:15:33 +0300

    hosting a simple website in an ec2 instance using apache web server

--- main.tf
+++ main.tf
@@ -10,14 +10,45 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region  = "eu-north-1"
+  profile = "default"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-08d70e59c07c61a3a"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = var.instance_name
+resource "aws_security_group" "vpc-web" {
+  name = "vpc-web"
+  description = "VPC Web"
+  ingress {
+    description = "Allow Port 80"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress{
+    description = "Allow Port 443"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress{
+    description = "Allow all ip and ports outbound"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0274f4b62b6ae3bd5"
+  instance_type = "t3.micro"
+  user_data = file("${path.module}/simpleWeb.sh")
+  tags = {
+    "Name" = "EC2 Demo"  
+  }
+  vpc_security_group_ids = [aws_security_group.vpc-web.id]
 }
